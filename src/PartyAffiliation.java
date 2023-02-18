3e17b0e8e73d88538f697554de06d5fb0c6de38b
Lucas <gibsonlc@mail.uc.edu>
2023-02-18 11:06:04 -0500

    Project init

--- src/PartyAffiliation.java
+++ src/PartyAffiliation.java
@@ -1,21 +1,28 @@
 public class PartyAffiliation {
     public static void main(String[] args) {
+        //Declare Variables
         String menu = "Please chose and enter one of the following options: D, R, or I";
         String userInput = "D";
+        //Prompt
         System.out.println(menu);
+        //Input
         System.out.println("Testing user input of "+userInput);
+        //Output If "D"
         if (userInput.equalsIgnoreCase("D"))
         {
             System.out.println("You get a Democratic Donkey");
         }
+        //Output If "R"
         else if(userInput.equalsIgnoreCase("R"))
         {
             System.out.println("You get a Republican Elephant");
         }
+        //Output If "I"
         else if (userInput.equalsIgnoreCase("I"))
         {
             System.out.println("You get an Independent Person");
         }
+        //Output If Invalid
         else
         {
             System.out.println(userInput+" is not a valid input");
